4e61fc104aa26ed1913a45f1c10a05d23b1ae34e
Marcel Lanz <marcellanz@n-1.ch>
2020-11-15 21:35:22 +0100

    [fix] choose new proxy image.

--- tck/run_dev_proxy.sh
+++ tck/run_dev_proxy.sh
@@ -5,13 +5,13 @@ function rnd() {
   cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1
 }
 
-PROXY_IMAGE=${2:-cloudstateio/cloudstate-proxy-dev-mode:latest}
+PROXY_IMAGE=${2:-cloudstateio/cloudstate-proxy-core:latest}
 PROXY="cloudstate-proxy-$(rnd)"
 
 set -x
 # run the proxy
 docker run --rm --name "$PROXY" -p 9000:9000 -e USER_FUNCTION_HOST=host.docker.internal -e USER_FUNCTION_PORT=8090 \
-  "${PROXY_IMAGE}" -Dcloudstate.proxy.passivation-timeout=30s || exit $?
+  "${PROXY_IMAGE}" -Dcloudstate.proxy.passivation-timeout=30s -Dconfig.resource=dev-mode.conf || exit $?
 tck_status=$?
 
 exit $tck_status
--- tck/run_dev_proxy_tck.sh
+++ tck/run_dev_proxy_tck.sh
@@ -5,7 +5,7 @@ function rnd() {
   cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1
 }
 
-PROXY_IMAGE=${2:-cloudstateio/cloudstate-proxy-dev-mode:latest}
+PROXY_IMAGE=${2:-cloudstateio/cloudstate-proxy-core:latest}
 PROXY="cloudstate-proxy-$(rnd)"
 TCK_IMAGE=${3:-cloudstateio/cloudstate-tck:latest}
 TCK="cloudstate-tck-$(rnd)"
@@ -16,7 +16,7 @@ finally() {
 trap finally EXIT
 set -x
 
-docker run -d --name "$PROXY" -p 9000:9000 -e USER_FUNCTION_HOST=host.docker.internal -e USER_FUNCTION_PORT=8090 "${PROXY_IMAGE}" || exit $?
+docker run -d --name "$PROXY" -p 9000:9000 -e USER_FUNCTION_HOST=host.docker.internal -e USER_FUNCTION_PORT=8090 "${PROXY_IMAGE}" -Dconfig.resource=dev-mode.conf || exit $?
 docker run --rm --name cloudstate-tck -p 8090:8090 -e TCK_HOST=0.0.0.0 -e TCK_PROXY_HOST=host.docker.internal -e TCK_SERVICE_HOST=host.docker.internal "${TCK_IMAGE}"
 tck_status=$?
 
